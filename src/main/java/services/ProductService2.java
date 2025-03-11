61e7c2d66af4c91abb15d6506d368852ca5764c4
HungLee104 <22130093@st.hcmuaf.edu.vn>
2025-03-11 20:56:23 +0700

    demo of servlet products

--- src/main/java/services/ProductService2.java
+++ src/main/java/services/ProductService2.java
@@ -8,9 +8,14 @@ import java.util.List;
 
 public class ProductService2 {
     private ProductDAO2 productDao;
-    public List<Product> getAllProducts(int nuPage, int nuPerPage) {
-        return productDao.getAllProducts(nuPage,nuPerPage);
+    public List<Product> getAllProducts(int nuPage, int nuPerPage, String option) {
+        return productDao.getAllProducts(nuPage,nuPerPage, option);
+    }
+    public List<Product> getProductByCategoryName(String nameCategory, int nuPage, int nuPerPage, String option) {
+        return productDao.getProductByCategoryName(nameCategory, nuPage, nuPerPage, option);
     }
 
-
+    public int getNuPage(int nuPerPage) {
+        return nuPerPage;
+    }
 }
