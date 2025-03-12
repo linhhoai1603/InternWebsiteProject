490ec648550954173fe8a45324cdfb3f92bc09d3
HungLee104 <22130093@st.hcmuaf.edu.vn>
2025-03-12 09:53:47 +0700

    improve snippet code to readable

--- src/main/java/controllers/ProductsServlet.java
+++ src/main/java/controllers/ProductsServlet.java
@@ -10,32 +10,31 @@ import models.Product;
 import services.ProductService2;
 import services.StyleService;
 
-@WebServlet(name = "ProductsServlet", value = "/products")
+@WebServlet(name = "ProductsServlet", value = "/product-zippers")
 public class ProductsServlet extends HttpServlet {
     int nuPerPage = 12;
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String option = request.getParameter("option");
-        String category = request.getParameter("category");
-
+        String selection = request.getParameter("selection");
         String param = request.getParameter("currentPage");
-        int currentPage = (param != null) ? Integer.parseInt(param) : 1;
+        String minPrice = request.getParameter("minPrice");
+        String maxPrice = request.getParameter("maxPrice");
 
+        int currentPage = (param != null) ? Integer.parseInt(param) : 1;
         if(option.isEmpty() || option == null) option = "Mới nhất";
 
-        List<Product> products;
         ProductService2 ps = new ProductService2();
-        int nupage = ps.getNuPage(nuPerPage);
-        if(category.isEmpty() || category == null){
-            products = ps.getAllProducts(currentPage,nuPerPage,option);
 
-        }else{
-            products = ps.getProductByCategoryName(category,currentPage,nuPerPage,option);
+        List<Product>products = ps.getProducts(selection,currentPage,nuPerPage,option, minPrice, maxPrice);
+        int nupage = ps.getNuPage(nuPerPage,selection);
 
-        }
         request.setAttribute("products", products);
         request.setAttribute("pageNumber", nupage);
         request.setAttribute("currentPage", currentPage);
         request.setAttribute("option", option);
+        request.setAttribute("selection", selection);
+        request.setAttribute("minPrice", minPrice);
+        request.setAttribute("maxPrice", maxPrice);
 
         request.getRequestDispatcher("zipstar-product.jsp").forward(request, response);
     }
