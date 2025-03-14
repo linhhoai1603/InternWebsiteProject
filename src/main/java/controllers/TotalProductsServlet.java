67c6368be0d399ceed596b912d5bcd25bccef3cf
HungLee104 <22130093@st.hcmuaf.edu.vn>
2025-03-14 16:01:02 +0700

    demo new product page

---
+++ src/main/java/controllers/TotalProductsServlet.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import java.io.*;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import models.Product;
+import services.ToTalProductService;
+
+@WebServlet(name = "TatolProductsServlet", value = "/total-product")
+public class TotalProductsServlet extends HttpServlet {
+    int nuPerPage = 12;
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String option = request.getParameter("option");
+        String selection = request.getParameter("selection");
+        String param = request.getParameter("currentPage");
+        String minPrice = request.getParameter("minPrice");
+        String maxPrice = request.getParameter("maxPrice");
+
+        int currentPage = (param != null) ? Integer.parseInt(param) : 1;
+        if(option.isEmpty() || option == null) option = "Mới nhất";
+
+        ToTalProductService ps = new ToTalProductService();
+
+        List<Product>products = ps.getProducts(selection,currentPage,nuPerPage,option, minPrice, maxPrice);
+        int nupage = ps.getNuPage(nuPerPage,selection);
+
+        request.setAttribute("products", products);
+        request.setAttribute("pageNumber", nupage);
+        request.setAttribute("currentPage", currentPage);
+        request.setAttribute("option", option);
+        request.setAttribute("selection", selection);
+        request.setAttribute("minPrice", minPrice);
+        request.setAttribute("maxPrice", maxPrice);
+
+        request.getRequestDispatcher("total-product.jsp").forward(request, response);
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        doGet(request, response);
+    }
+
+}
