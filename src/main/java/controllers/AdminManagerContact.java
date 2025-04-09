86462f156f8452c72f62d5bedbc81c9594f51365
leedhung <lehung.bh141@gmail.com>
2025-04-09 09:48:55 +0700

    update db and entities

--- src/main/java/controllers/AdminManagerContact.java
+++ src/main/java/controllers/AdminManagerContact.java
@@ -5,15 +5,21 @@ import java.io.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import models.ContactInfo;
+import services.ContactInfoService;
 import services.DeliveryService;
 
 @WebServlet(name = "AdminManagerContact", value = "/admin/manager-Contact")
 public class AdminManagerContact extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String email = request.getParameter("email");
-        String linkWebsite = request.getParameter("website");
-        String hotline = request.getParameter("hotline");
+        String message = request.getParameter("message");
+        ContactInfoService cis = new ContactInfoService();
+        ContactInfo ci = cis.getContactInfo(1);
+
+        request.setAttribute("message",message);
+        request.setAttribute("contactInfo", ci);
+        request.getRequestDispatcher("/admin/manager-contact.jsp").forward(request, response);
 
     }
 
--- src/main/java/services/ContactInfoService.java
+++ src/main/java/services/ContactInfoService.java
@@ -3,6 +3,7 @@ package services;
 import dao.AddressDao;
 import dao.CategoryDao;
 import dao.ContactDao;
+import models.Address;
 import models.ContactInfo;
 
 public class ContactInfoService {
@@ -13,7 +14,11 @@ public class ContactInfoService {
         addressDao= new AddressDao();
     }
     public ContactInfo getContactInfo(int contactId) {
-        return contactDao.getContactById(contactId);
+        int idAddress = contactDao.getIdAddress(contactId);
+        Address address = addressDao.getAddressByID(idAddress);
+        ContactInfo rs = contactDao.getContactById(contactId);
+        rs.setAddress(address);
+        return rs ;
     }
     public boolean updateInfContact(int idContact, String email, String website, String hotline) {
         return contactDao.updateInfContact(idContact, email, website, hotline);
