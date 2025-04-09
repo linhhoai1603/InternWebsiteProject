6e64092af0b943a14075e3bb168443120ba58ff8
leedhung <lehung.bh141@gmail.com>
2025-04-09 13:46:54 +0700

    covert to gradle

--- src/main/java/dao/ToTalProductDAO.java
+++ src/main/java/dao/ToTalProductDAO.java
@@ -15,35 +15,26 @@ public class ToTalProductDAO {
         jdbi = DBConnection.getConnetion();
     }
     public List<Product> getAllProducts(int currentPage, int nuPerPage, String option) {
-//        if(option.isBlank() || option.isEmpty() ){
-//            option = "Bán chạy";
-//        }
-//        String condition = "1=1";
-//        switch (option) {
-//            case "Bán chạy":
-//                condition = "";
-//
-//        }
         int offset = (currentPage - 1) * nuPerPage;
         String sql = """
-        SELECT
-            p.id, p.name, p.quantity, p.addedDate, p.description, p.area, p.selling, p.img,
-            c.id AS category_id, c.name AS category_name,
-            pr.id AS price_id, pr.price, pr.discountPercent, pr.lastPrice,
-            ti.id AS technical_info_id, ti.specifications, ti.manufactureDate
-        FROM
-            products p
-        JOIN
-            categories c ON p.idCategory = c.id
-        JOIN
-            prices pr ON p.idPrice = pr.id
-        LEFT JOIN
-            technical_information ti ON p.idTechnical = ti.id
-        ORDER BY p.id
-        LIMIT :nuPerPage OFFSET :offset
+    SELECT
+        p.id, p.name, p.quantity, p.addedDate, p.description, 
+        p.height, p.weight, p.width, p.selling, p.img,
+        c.id AS category_id, c.name AS category_name,
+        pr.id AS price_id, pr.price, pr.discountPercent, pr.lastPrice,
+        ti.id AS technical_info_id, ti.specifications, ti.manufactureDate, ti.releaseDay
+    FROM
+        products p
+    JOIN
+        categories c ON p.idCategory = c.id
+    JOIN
+        prices pr ON p.idPrice = pr.id
+    LEFT JOIN
+        technical_information ti ON p.idTechnical = ti.id
+    ORDER BY p.id
+    LIMIT :nuPerPage OFFSET :offset
     """;
 
-        //String finalCondition = condition;
         return jdbi.withHandle(handle ->
                 handle.createQuery(sql)
                         .bind("nuPerPage", nuPerPage)
@@ -55,7 +46,9 @@ public class ToTalProductDAO {
                             product.setQuantity(rs.getInt("quantity"));
                             product.setDateAdded(rs.getDate("addedDate").toLocalDate());
                             product.setDescription(rs.getString("description"));
-                            product.setArea(rs.getDouble("area"));
+                            product.setHeight(rs.getDouble("height")); // Thêm trường height
+                            product.setWeight(rs.getDouble("weight")); // Thêm trường weight
+                            product.setWidth(rs.getDouble("width"));   // Thêm trường width
                             product.setSelling(rs.getInt("selling"));
                             product.setImage(rs.getString("img"));
 
@@ -68,6 +61,7 @@ public class ToTalProductDAO {
                             technicalInfo.setId(rs.getInt("technical_info_id"));
                             technicalInfo.setSpecification(rs.getString("specifications"));
                             technicalInfo.setManufactureDate(rs.getDate("manufactureDate").toLocalDate());
+                            technicalInfo.setReleaseDay(rs.getDate("releaseDay").toLocalDate());
                             product.setTechnicalInfo(technicalInfo);
 
                             Price price = new Price();
@@ -82,26 +76,29 @@ public class ToTalProductDAO {
                         .list()
         );
     }
-    public List<Product> getProductByCategoryName(String selection, int currentPage, int nuPerPage , String option) {
+
+
+    public List<Product> getProductByCategoryName(String selection, int currentPage, int nuPerPage, String option) {
         int offset = (currentPage - 1) * nuPerPage;
         String sql = """
-        SELECT
-            p.id, p.name, p.quantity, p.addedDate, p.description, p.area, p.selling, p.img,
-            c.id AS category_id, c.name AS category_name,
-            pr.id AS price_id, pr.price, pr.discountPercent, pr.lastPrice,
-            ti.id AS technical_info_id, ti.specifications, ti.manufactureDate
-        FROM
-            products p
-        JOIN
-            categories c ON p.idCategory = c.id
-        JOIN
-            prices pr ON p.idPrice = pr.id
-        LEFT JOIN
-            technical_information ti ON p.idTechnical = ti.id
-        WHERE 
-            c.name = :nameCategory
-        ORDER BY p.id
-        LIMIT :nuPerPage OFFSET :offset
+    SELECT
+        p.id, p.name, p.quantity, p.addedDate, p.description, 
+        p.height, p.weight, p.width, p.selling, p.img,
+        c.id AS category_id, c.name AS category_name,
+        pr.id AS price_id, pr.price, pr.discountPercent, pr.lastPrice,
+        ti.id AS technical_info_id, ti.specifications, ti.manufactureDate,ti.releaseDay
+    FROM
+        products p
+    JOIN
+        categories c ON p.idCategory = c.id
+    JOIN
+        prices pr ON p.idPrice = pr.id
+    LEFT JOIN
+        technical_information ti ON p.idTechnical = ti.id
+    WHERE 
+        c.name = :nameCategory
+    ORDER BY p.id
+    LIMIT :nuPerPage OFFSET :offset
     """;
 
         return jdbi.withHandle(handle ->
@@ -116,7 +113,9 @@ public class ToTalProductDAO {
                             product.setQuantity(rs.getInt("quantity"));
                             product.setDateAdded(rs.getDate("addedDate").toLocalDate());
                             product.setDescription(rs.getString("description"));
-                            product.setArea(rs.getDouble("area"));
+                            product.setHeight(rs.getDouble("height")); // Thêm trường height
+                            product.setWeight(rs.getDouble("weight")); // Thêm trường weight
+                            product.setWidth(rs.getDouble("width"));   // Thêm trường width
                             product.setSelling(rs.getInt("selling"));
                             product.setImage(rs.getString("img"));
 
@@ -129,6 +128,7 @@ public class ToTalProductDAO {
                             technicalInfo.setId(rs.getInt("technical_info_id"));
                             technicalInfo.setSpecification(rs.getString("specifications"));
                             technicalInfo.setManufactureDate(rs.getDate("manufactureDate").toLocalDate());
+                            technicalInfo.setReleaseDay(rs.getDate("releaseDay").toLocalDate());
                             product.setTechnicalInfo(technicalInfo);
 
                             Price price = new Price();
@@ -142,8 +142,9 @@ public class ToTalProductDAO {
                         })
                         .list()
         );
-
     }
+
+
     public int countProducts() {
         return jdbi.withHandle(handle ->
                 handle.createQuery("SELECT COUNT(*) FROM products")
@@ -165,24 +166,25 @@ public class ToTalProductDAO {
     public List<Product> getProductByPriceRange(String selection, int currentPage, int nuPerPage, String option, String minPrice, String maxPrice) {
         int offset = (currentPage - 1) * nuPerPage;
         String sql = """
-        SELECT
-            p.id, p.name, p.quantity, p.addedDate, p.description, p.area, p.selling, p.img,
-            c.id AS category_id, c.name AS category_name,
-            pr.id AS price_id, pr.price, pr.discountPercent, pr.lastPrice,
-            ti.id AS technical_info_id, ti.specifications, ti.manufactureDate
-        FROM
-            products p
-        JOIN
-            categories c ON p.idCategory = c.id
-        JOIN
-            prices pr ON p.idPrice = pr.id
-        LEFT JOIN
-            technical_information ti ON p.idTechnical = ti.id
-        WHERE 
-            c.name = :nameCategory
-            AND pr.price BETWEEN :minPrice AND :maxPrice
-        ORDER BY p.id
-        LIMIT :nuPerPage OFFSET :offset
+    SELECT
+        p.id, p.name, p.quantity, p.addedDate, p.description, 
+        p.height, p.weight, p.width, p.selling, p.img,
+        c.id AS category_id, c.name AS category_name,
+        pr.id AS price_id, pr.price, pr.discountPercent, pr.lastPrice,
+        ti.id AS technical_info_id, ti.specifications, ti.manufactureDate,ti.releaseDay
+    FROM
+        products p
+    JOIN
+        categories c ON p.idCategory = c.id
+    JOIN
+        prices pr ON p.idPrice = pr.id
+    LEFT JOIN
+        technical_information ti ON p.idTechnical = ti.id
+    WHERE 
+        c.name = :nameCategory
+        AND pr.price BETWEEN :minPrice AND :maxPrice
+    ORDER BY p.id
+    LIMIT :nuPerPage OFFSET :offset
     """;
 
         return jdbi.withHandle(handle ->
@@ -199,7 +201,9 @@ public class ToTalProductDAO {
                             product.setQuantity(rs.getInt("quantity"));
                             product.setDateAdded(rs.getDate("addedDate").toLocalDate());
                             product.setDescription(rs.getString("description"));
-                            product.setArea(rs.getDouble("area"));
+                            product.setHeight(rs.getDouble("height")); // Thêm trường height
+                            product.setWeight(rs.getDouble("weight")); // Thêm trường weight
+                            product.setWidth(rs.getDouble("width"));   // Thêm trường width
                             product.setSelling(rs.getInt("selling"));
                             product.setImage(rs.getString("img"));
 
@@ -212,6 +216,7 @@ public class ToTalProductDAO {
                             technicalInfo.setId(rs.getInt("technical_info_id"));
                             technicalInfo.setSpecification(rs.getString("specifications"));
                             technicalInfo.setManufactureDate(rs.getDate("manufactureDate").toLocalDate());
+                            technicalInfo.setReleaseDay(rs.getDate("releaseDay").toLocalDate());
                             product.setTechnicalInfo(technicalInfo);
 
                             Price price = new Price();
@@ -226,6 +231,7 @@ public class ToTalProductDAO {
                         .list()
         );
     }
+
 
 
     public List<Product> getAllProductByPriceRange( int currentPage, int nuPerPage, String option, String minPrice, String maxPrice) {
